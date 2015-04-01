eab6c384bea9530e6e5e1117a5355f112280cffc
Enol Fernandez <enol.fernandez@egi.eu>
2015-04-01 11:13:11 +0200

    Some fixes.

--- umd/products/storm.py
+++ umd/products/storm.py
@@ -10,8 +10,8 @@ class StormSL5Deploy(Deploy):
     def pre_install(self):
         print(yellow("PRE-install actions."))
 
-        local("/usr/sbin/adduser -M storm")
-        local("/usr/sbin/adduser -M gridhttps")
+        #local("/usr/sbin/adduser -M storm")
+        #local("/usr/sbin/adduser -M gridhttps")
 
         print(green("users storm and gridhttps added"))
         print(yellow("END of PRE-install actions."))
@@ -22,16 +22,18 @@ class StormSL5Deploy(Deploy):
         self.pkgtool.install(pkgs=["ntp", "ca-policy-egi-core"])
         print(green("<ntp, ca-policy-egi-core> installed."))
 
-        local("mount -o remount,acl,user_xattr /")
-        print(green("Enabled ACLs and Extended Attribute Support in /"))
+        #local("mount -o remount,acl,user_xattr /")
+        #print(green("Enabled ACLs and Extended Attribute Support in /"))
 
         print(yellow("END of PRE-config actions."))
 
 
 sl5 = StormSL5Deploy(
     name="storm-sl5",
+    need_cert=True,
     metapkg=["emi-storm-backend-mp", "emi-storm-frontend-mp",
              "emi-storm-globus-gridftp-mp", "emi-storm-gridhttps-mp"],
     nodetype=["se_storm_backend",  "se_storm_frontend", "se_storm_gridftp",
               "se_storm_gridhttps"],
-    siteinfo=["site-info-storm.def"])
+    siteinfo=["site-info-storm.def"],
+    validate_path=[("bin/storm", {"user": "umd"})])
