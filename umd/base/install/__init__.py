62a6c87295c2cb075a1e1b0767fba6f13cc22aa9
Pablo Orviz <orviz@ifca.unican.es>
2015-05-06 17:39:18 +0200

    Make verification repository URL optional.

--- umd/base/install/__init__.py
+++ umd/base/install/__init__.py
@@ -33,19 +33,19 @@ class Install(object):
 
     def run(self,
             installation_type,
-            repository_url,
             epel_release_url,
             umd_release_url,
-            *args, **kwargs):
+            repository_url=None,
+            **kwargs):
         """Runs UMD installation.
 
            Arguments::
                 installation_type: install from scratch ('install') or
                                    update ('update')
-                repository_url: base repository URL
-                                (with the verification stuff).
                 epel_release_url: EPEL release (URL).
                 umd_release_url : UMD release (URL).
+                repository_url: base repository URL
+                                (with the verification stuff).
         """
         if installation_type == "update":
             qc_step = QCStep("QC_UPGRADE_1", "Upgrade", "/tmp/qc_upgrade_1")
@@ -99,7 +99,9 @@ class Install(object):
                                   % self.metapkg)
 
             # 2) Enable verification repository
-            self._enable_verification_repo(qc_step, repository_url)
+	    if repository_url:
+		info("Verification repository provided.")
+            	self._enable_verification_repo(qc_step, repository_url)
 
             # 3) Update
             r = self.pkgtool.update()
@@ -113,7 +115,9 @@ class Install(object):
                                      msg="System successfully updated.")
         elif installation_type == "install":
             # 1) Enable verification repository
-            self._enable_verification_repo(qc_step, repository_url)
+	    if repository_url:
+		info("Verification repository provided.")
+            	self._enable_verification_repo(qc_step, repository_url)
 
             # 2) Install verification version
             r = self.pkgtool.install(self.metapkg)
